1c635d73f9506f6044ed24de6338d5d1bd1639a2
Duarte <joaoduarte227@gmail.com>
2019-06-05 12:50:20 +0100

    SQLITE31

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ def gravar(v1, v2):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizador.db')
     db = ficheiro.cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS usr (usr texte, pwd text)")
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr text, pwd text)")
     db.execute("INSERT INTO usr VALUES (?, ?)", (v1, v2))
     ficheiro.commit()
     ficheiro.close()
     return
+
 
 def alterar(v1, v2):
     import sqlite3
@@ -22,18 +23,32 @@ def alterar(v1, v2):
     ficheiro.close()
     return
 
+
+def existe(v1):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizador.db')
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE usr = ?", (v1,))
+    valor = db.fetchone()
+    ficheiro.close()
+    return valor
+
+
 @app.route('/', methods=['POST', 'GET'])
-def index():
+def registo():
     erro = None
     if request.method == "POST":
         v1 = request.form['usr']
         v2 = request.form['pwd']
         v3 = request.form['cpwd']
-        if v2 != v3:
-            erro = 'A passe não coincide.'
+        if existe(v1):
+            erro = 'O utilizador já existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
         else:
-            alterar(v1, v2)
-    return render_template('Newpass.html', erro=erro)
+            gravar(v1, v2)
+            erro = 'Utilizador registado.'
+    return render_template('registo.html', erro=erro)
 
 
 if __name__ == '__main__':
